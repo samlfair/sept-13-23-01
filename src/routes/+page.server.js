7c60bd4aacce596b261d2779f6c186f2873e06f1
Sam Littlefair <sam.littlefair@prismic.io>
2023-09-13 18:42:06 +0200

    remove prerender

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,8 +2,6 @@ import { asText } from '@prismicio/client';
 
 import { createClient } from '$lib/prismicio';
 
-export const prerender = true;
-
 export async function load() {
 	const client = createClient();
 
